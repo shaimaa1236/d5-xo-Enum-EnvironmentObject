401e977dc51fdb0dbe0c8554609cda491311dcd6
shaimaa <sh1611111144@outlook.com>
2020-05-10 10:53:24 +0300

    Initial Commit

---
+++ d5-xo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  d5-xo
+//
+//  Created by Shaimaa on 5/10/20.
+//  Copyright © 2020 Shaimaa. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
